4e8026c828dce483a6100486762b9101d66f5c3c
Mohamed Ibrahim <mibrahim.dev@gmail.com>
2020-03-13 23:13:37 +0300

    implement coroutine scope in base fragment

--- app/src/main/java/com/morihacky/android/rxjava/fragments/BaseFragment.kt
+++ app/src/main/java/com/morihacky/android/rxjava/fragments/BaseFragment.kt
@@ -2,9 +2,11 @@ package com.morihacky.android.rxjava.fragments
 
 import androidx.fragment.app.Fragment
 import com.morihacky.android.rxjava.MyApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 
-open class BaseFragment : Fragment() {
-    
+open class BaseFragment : Fragment(), CoroutineScope by CoroutineScope(Dispatchers.Main) {
+
     override fun onDestroy() {
         super.onDestroy()
         val refWatcher = MyApp.getRefWatcher()
